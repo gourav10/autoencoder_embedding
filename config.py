4033e9354db92b81c0edf8458e44b7ba83790f82
Gourav10 <bueragourav@gmail.com>
2022-11-29 00:53:06 -0500

    Added scripts to train model

---
+++ config.py
@@ -0,0 +1,7 @@
+DATA_DIR = "data"
+
+NUM_WORKERS = 0
+
+# how many samples per batch to load
+BATCH_SIZE = 20
+NUM_EPOCHS = 30
---
+++ engine.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import config
+from model import Encoder, Decoder
+
+
+class Engine:
+    def __init__(self,
+                 encoded_space_dim,
+                 num_epochs=config.NUM_EPOCHS) -> None:
+        if (torch.cuda.is_available()):
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
+        torch.manual_seed(0)
+        self.encoder = Encoder(encoded_space_dim=encoded_space_dim,
+                               fc2_input_dim=128)
+        self.decoder = Decoder(encoded_space_dim=encoded_space_dim)
+        self.num_epochs = num_epochs
+
+    def __train(self, dataloader, loss_fn, optimizer):
+        self.encoder.train()
+        self.decoder.train()
+        train_loss = []
+        for image_batch, _ in dataloader:
+            image_batch = image_batch.to(self.device)
+            encoded_data = self.encoder(image_batch)
+            decoded_data = self.decoder(encoded_data)
+            loss = loss_fn(decoded_data, image_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print('\t partial train loss (single batch): %f' % (loss.data))
+            train_loss.append(loss.detach().cpu().numpy())
+        return np.mean(train_loss)
+
+    def __validate(self, dataloader, loss_fn):
+        self.encoder.eval()
+        self.decoder.eval()
+        with torch.no_grad():
+            conc_out = []
+            conc_label = []
+            for image_batch, _ in dataloader:
+                image_batch = image_batch.to(self.device)
+                encoded_data = self.encoder(image_batch)
+                decoded_data = self.decoder(encoded_data)
+                conc_out.append(decoded_data.cpu())
+                conc_label.append(image_batch.cpu())
+            conc_out = torch.cat(conc_out)
+            conc_label = torch.cat(conc_label)
+            val_loss = loss_fn(conc_out, conc_label)
+        return val_loss.data
+
+    def train_model(self, train_dl, test_dl, loss_fn, optimizer):
+        history = {'train_loss': [], 'val_loss': []}
+        for epoch in range(self.num_epochs):
+            train_loss = self.__train(train_dl, loss_fn, optimizer)
+            val_loss = self.__validate(test_dl, loss_fn)
+            if (epoch % 5 == 0):
+                print('\n EPOCH {}/{} \t train loss{:.3f} \t val loss {:.3f}'
+                      .format(epoch + 1, self.num_epochs,
+                              train_loss, val_loss))
+            history['train_loss'].append(train_loss)
+            history['val_loss'].append(val_loss)
+        return history
+
+    def plot_train_loss(self, history):
+        plt.figure(figsize=(10, 8))
+        plt.semilogy(history['train_loss'], label='Train')
+        plt.semilogy(history['val_loss'], label='Valid')
+        plt.xlabel('Epoch')
+        plt.ylabel('Average Loss')
+        plt.show()
